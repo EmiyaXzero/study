751ab2d70da20dc6de2e174e60e55265fd33d1f0
shanghang <825564615@qq.com>
2019-12-13 17:51:55 +0800

    修改超时时间，加上log4j配置观看日志

--- studyzk/src/main/java/com/my/zk/DataMonitor.java
+++ studyzk/src/main/java/com/my/zk/DataMonitor.java
@@ -1,26 +1,93 @@
 package com.my.zk;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Arrays;
 
 /**
  * @author shang
  */
-public class DataMonitor {
+public class DataMonitor implements Watcher, AsyncCallback.StatCallback {
     private ZooKeeper zk;
     private String zNode;
     private Watcher watcher;
     private Executor executor;
-    public boolean dead =false;
+    DataMonitorListener listener;
+    private byte[] prevData;
+    public boolean dead;
     public DataMonitor(ZooKeeper zk, String zNode, Watcher watcher, Executor executor) {
         this.zk = zk;
         this.zNode = zNode;
         this.watcher = watcher;
         this.executor = executor;
+        zk.exists(zNode,true,this,null);
     }
 
     public void process(WatchedEvent watchedEvent) {
+        String path = watchedEvent.getPath();
+        if(watchedEvent.getType() == Watcher.Event.EventType.None){
+            switch (watchedEvent.getState()){
+                case SyncConnected:
+                    // In this particular example we don't need to do anything
+                    // here - watches are automatically re-registered with
+                    // server and any watches triggered while the client was
+                    // disconnected will be delivered (in order of course)
+                    return;
+                case Expired:
+                    dead = true;
+                    listener.closing(KeeperException.Code.SessionExpired);
+                    break;
+                default:
+                    break;
+            }
+        }else{
+            if(path != null && path.equals(zNode)){
+                zk.exists(zNode,true,this,null);
+            }
+        }
+        if(watcher != null){
+            watcher.process(watchedEvent);
+        }
+    }
+
+    public void processResult(int i, String s, Object o, Stat stat) {
+        boolean exists;
+        switch (i){
+            case KeeperException.Code
+                    .Ok:
+                exists = true;
+                break;
+            case KeeperException.Code
+                        .NoNode:
+                exists=false;
+                break;
+            case KeeperException.Code.SessionExpired:
+            case KeeperException.Code.NoAuth:
+                dead = true;
+                listener.closing(i);
+                return;
+            default:
+                zk.exists(zNode,true,this,null);
+                return;
+        }
+        byte[] b = null;
+        if(exists){
+            try {
+                b = zk.getData(zNode, false, null);
+            } catch (KeeperException e) {
+                // We don't need to worry about recovering now. The watch
+                // callbacks will kick off any exception handling
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        boolean isTrue = (b == null && b!= prevData) || (b!=null && !Arrays.equals(prevData,b));
+        if(isTrue){
+            listener.exists(b);
+            prevData = b;
+        }
     }
 
     public interface DataMonitorListener {
@@ -28,7 +95,7 @@ public class DataMonitor {
          * zNode在zookeeper存在时候
          * @param data
          */
-        void exists(byte data[]);
+        void exists(byte[] data);
 
         /**
          * zookeeper连接关闭的时候
--- studyzk/src/main/java/com/my/zk/Executor.java
+++ studyzk/src/main/java/com/my/zk/Executor.java
@@ -4,29 +4,37 @@ import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * ZooKeeper官方文档第一个demo
  * @author shang
  */
 public class Executor implements Watcher ,Runnable,DataMonitor.DataMonitorListener {
+    private static Logger log = LoggerFactory.getLogger(Executor.class);
     private String  filename;
     private String[] exec;
     private ZooKeeper zk;
     private DataMonitor dm;
+    private Process child;
     public static void main(String[] args) {
-        if (args.length < 4) {
-            System.err
-                    .println("USAGE: Executor hostPort znode filename program [args ...]");
-            System.exit(2);
+        Properties properties = new Properties();
+        InputStream in = Executor.class.getClassLoader().getResourceAsStream("zkServer.properties");
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            log.error("缺少配置文件");
         }
-        String hostPort = args[0];
-        String zNode = args[1];
-        String filename = args[2];
-        String[] exec = new String[args.length - 3];
-        System.arraycopy(args, 3, exec, 0, exec.length);
+        String hostPort = properties.getProperty("hostPort");
+        String zNode = properties.getProperty("zNode");
+        String filename = properties.getProperty("filename");
+        String[] exec = new String[args.length];
         try {
             new Executor(hostPort, zNode, filename, exec).run();
         } catch (Exception e) {
@@ -34,12 +42,13 @@ public class Executor implements Watcher ,Runnable,DataMonitor.DataMonitorListen
         }
     }
 
-    public Executor(String hostPort, String znode, String filename,
+    public Executor(String hostPort, String zNode, String filename,
                     String[] exec) throws KeeperException, IOException {
         this.filename = filename;
         this.exec = exec;
-        zk = new ZooKeeper(hostPort, 3000, this);
-        dm = new DataMonitor(zk, znode, null, this);
+        //zk超时时间调大点，原来用的3000连接失败无法走进process方法，调成10000后watch到连接后走进process
+        zk = new ZooKeeper(hostPort, 10000, this);
+        dm = new DataMonitor(zk, zNode, null, this);
     }
 
     public void run() {
@@ -54,14 +63,66 @@ public class Executor implements Watcher ,Runnable,DataMonitor.DataMonitorListen
     }
 
     public void exists(byte[] data) {
+        if(data == null){
+            if(child != null){
+                child.destroy();
+                try{
+                    child.waitFor();
+                }catch(InterruptedException e){
 
+                }
+            }
+            child = null;
+        }else{
+            if(child != null){
+                child.destroy();
+                try{
+                    child.waitFor();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+            try{
+                FileOutputStream fos = new FileOutputStream(filename);
+                fos.write(data);
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void closing(int rc) {
-
+        synchronized (this){
+            notifyAll();
+        }
     }
 
     public void process(WatchedEvent watchedEvent) {
         dm.process(watchedEvent);
+    }
+
+    static class SteamWriter extends Thread{
+        OutputStream os;
+        InputStream is;
+
+        SteamWriter(InputStream is, OutputStream os){
+            this.is = is;
+            this.os = os;
+            start();
+        }
+
+        @Override
+        public void run(){
+            byte[] b = new byte[80];
+            int rc ;
+            try{
+                while ((rc = is.read(b))>0){
+                    os.write(b, 0, rc);
+                }
+            }catch (IOException e){
+
+            }
+        }
     }
 }
