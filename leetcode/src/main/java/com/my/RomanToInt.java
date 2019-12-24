185a83922523f209a0aafbb3ff978cfba55ca4ea
shanghang <825564615@qq.com>
2019-12-24 17:19:29 +0800

    优化下用char

--- leetcode/src/main/java/com/my/RomanToInt.java
+++ leetcode/src/main/java/com/my/RomanToInt.java
@@ -8,43 +8,43 @@ import java.util.Map;
  * @author shanghang
  */
 public class RomanToInt {
-    static Map<String,Integer> roman = new HashMap<String,Integer>(8){
+    static Map<Character,Integer> roman = new HashMap<Character,Integer>(8){
         {
-            put("I", 1);
-            put("V", 5);
-            put("X", 10);
-            put("L", 50);
-            put("C", 100);
-            put("D", 500);
-            put("M", 1000);
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
         }
     };
 
     public static int romanToInt(String s) {
         int result = 0;
         if(s.length() == 1){
-            return roman.get(s.substring(0,1));
+            return roman.get(s.charAt(0));
         }
         //如果左边数字比右边小则两个数字整体为 右边减左边
         int i = 0;
         while (i<s.length()){
             if((i+1)<s.length()){
-                if(judge(s.substring(i,i+1),s.substring(i+1,i+2))){
-                    result+=roman.get(s.substring(i,i+1));
+                if(judge(s.charAt(i),s.charAt(i+1))){
+                    result+=roman.get(s.charAt(i));
                     i++;
                 }else {
-                    result+=roman.get(s.substring(i+1,i+2)) - roman.get(s.substring(i,i+1));
+                    result+=roman.get(s.charAt(i+1)) - roman.get(s.charAt(i));
                     i+=2;
                 }
             }else{
-                result+=roman.get(s.substring(i,i+1));
+                result+=roman.get(s.charAt(i));
                 i++;
             }
         }
         return result;
     }
 
-    public static boolean judge(String a, String b){
+    public static boolean judge(char a, char b){
         return roman.get(a)>=roman.get(b);
     }
 
