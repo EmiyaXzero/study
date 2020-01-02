ed2e5d4bbaad63f74851d994e19e93b28227dffd
shanghang <9LxjeqhOxS5A>
2020-01-02 21:27:07 +0800

    加一

--- leetcode/src/main/java/com/my/PlusOne.java
+++ leetcode/src/main/java/com/my/PlusOne.java
@@ -7,15 +7,30 @@ import java.util.ArrayList;
  * @author shang
  */
 public class PlusOne {
-    public int[] plusOne(int[] digits) {
-        //防止digits+1溢出了比如9999+1  10000
+    public static int[] plusOne(int[] digits) {
+        //防止digits+1溢出了比如9999+1  10000,result不一定能用到
         int[] result = new int[digits.length+1];
         //先把digits转成int
         int length = digits.length;
-        boolean isAdd = false;
-        for (int i = length-1; i >= 0; i--) {
-            if()
+        int end = 1;
+        for (int i = length - 1 ;i>=0 ;i--){
+            int add = digits[i]+1;
+            if(add > 9){
+                digits[i] = 0;
+            }else {
+                digits[i] = add;
+                return digits;
+            }
         }
+        //假如到这里还没return
+        result[0] = 1;
+        for (int i = 1;i<length+1;i++){
+            result[i] = 0;
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(plusOne(new int[]{ 9}));
     }
 }
