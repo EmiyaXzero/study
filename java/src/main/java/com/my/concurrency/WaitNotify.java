498a30c68b62629f80f0e6fb4f3b176765540b8e
shanghang <9LxjeqhOxS5A>
2020-01-08 22:54:21 +0800

    wait是指在一个已经进入了同步锁的线程内,让自己暂时让出同步锁 notify并不释放锁，只是告诉调用过wait方法的线程可以去参与获得锁的竞争了，但不是马上得到锁，因为锁还在别人手里，别人还没释放

---
+++ java/src/main/java/com/my/concurrency/WaitNotify.java
@@ -0,0 +1,77 @@
+package com.my.concurrency;
+
+import org.apache.log4j.Logger;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author shanghang
+ */
+public class WaitNotify {
+    static  Logger logger = Logger.getLogger(WaitNotify.class);
+    static boolean flag = true;
+    static Object lock = new Object();
+
+    public static void main(String[] args) {
+        Thread waitThread = new Thread(new Wait(),"waitThread");
+        waitThread.start();
+        SleepUtils.second(1);
+        Thread notifyThread = new Thread(new Notify(),"NotifyThread");
+        notifyThread.start();
+    }
+
+    static class Wait implements Runnable{
+
+        @Override
+        public void run() {
+            synchronized (lock){
+                while(flag){
+                    try{
+                        logger.error(Thread.currentThread() + " flag is true. wait @  "
+                                +new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                        //wait是指在一个已经进入了同步锁的线程内,让自己暂时让出同步锁
+                        lock.wait();
+                    }catch (Exception e){
+
+                    }
+                }
+                logger.error(Thread.currentThread() + " flag is false. running @  "
+                        +new SimpleDateFormat("HH:mm:ss").format(new Date()));
+            }
+        }
+    }
+
+    static class Notify implements Runnable{
+
+        @Override
+        public void run() {
+            synchronized (lock){
+                logger.error(Thread.currentThread() + " hold lock. notify @  "
+                        +new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                //notify并不释放锁,只是告诉调用过wait方法的线程可以去参与获得锁的竞争了,但不是马上得到锁,因为锁还在别人手里,别人还没释放
+                lock.notifyAll();
+                flag = false;
+                SleepUtils.second(5);
+            }
+            SleepUtils.second(5);
+            synchronized (lock){
+                logger.error(Thread.currentThread() + " hold again. notify @  "
+                        +new SimpleDateFormat("HH:mm:ss").format(new Date()));
+                SleepUtils.second(10);
+            }
+
+        }
+    }
+
+    static class SleepUtils {
+        public static final void second(long seconds) {
+            try {
+                TimeUnit.SECONDS.sleep(seconds);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
