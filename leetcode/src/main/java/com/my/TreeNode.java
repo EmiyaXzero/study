37282e973cc6d44f4343b646845881c023ee94cc
shanghang <9LxjeqhOxS5A>
2020-01-15 21:42:39 +0800

    不同的二叉搜索树 II

---
+++ leetcode/src/main/java/com/my/TreeNode.java
@@ -0,0 +1,13 @@
+package com.my;
+
+/**
+ * @Author: shanghang
+ * @Project:study
+ * @Date: 2020/1/15 20:41
+ **/
+public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+}
