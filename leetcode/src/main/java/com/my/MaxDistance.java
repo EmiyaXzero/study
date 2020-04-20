752a4e03d99a841f9dc992d279c41a578087ad60
shanghang <825564615@qq.com>
2020-04-20 21:29:41 +0800

    岛屿数量这么做肯定有问题

---
+++ leetcode/src/main/java/com/my/MaxDistance.java
@@ -0,0 +1,57 @@
+package com.my;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author shang
+ * @title: MaxDistance
+ * @projectName study
+ * @description: 地图分析
+ * @date 2020/4/20-19:02
+ */
+public class MaxDistance {
+    public int maxDistance(int[][] grid) {
+        //将岛屿放到队列里面
+        int max = -1;
+        int[][] visted = new int[grid.length][grid[0].length];
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i <grid.length ; i++) {
+            for (int j = 0 ;j<grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    queue.add(new int[]{i,j});
+                    //岛屿的步骤为0
+                    grid[i][j] = 0;
+                    //表示访问过
+                    visted[i][j] = 1;
+                }
+            }
+        }
+        int[] dx = new int[] {-1, 1, 0, 0};
+        int[] dy = new int[] {0, 0, -1, 1};
+        while (!queue.isEmpty()){
+            //出队
+            int[] curIdx = queue.poll();
+            int curX = curIdx[0];
+            int curY = curIdx[1];
+            //将当前节点置为已访问
+            for (int i = 0 ;i<4;i++){
+                int tempX = curX+dx[i];
+                int tempY = curY+dy[i];
+
+                if(tempX>=0 && tempX<=grid.length-1 && tempY>=0 && tempY<= grid[0].length-1 && visted[tempX][tempY] == 0){
+                    visted[curIdx[0]][curIdx[1]] = 1;
+                    grid[tempX][tempY] = grid[curX][curY]+1;
+                    max = Math.max(max, grid[tempX][tempY]);
+                    queue.add(new int[]{tempX,tempY});
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaxDistance maxDistance = new MaxDistance();
+        maxDistance.maxDistance(new int[][]{{0,0,1,1,1},{0,1,1,0,0},{0,0,1,1,0},{1,0,0,0,0},{1,1,0,0,1}});
+    }
+}
--- leetcode/src/main/java/com/my/NumIslands.java
+++ leetcode/src/main/java/com/my/NumIslands.java
@@ -2,7 +2,6 @@ package com.my;
 
 
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -163,6 +162,60 @@ public class NumIslands {
         }
         return set.size();
     }
+
+    /**
+     * BFS 深度优先算法解决岛屿问题
+     * @param
+     */
+    public int numIslandsByBfs(char[][] grid) {
+        if (grid.length <= 0 || grid[0].length <= 0) {
+            return 0;
+        }
+        int count=0;
+        for (int i = 0; i <grid.length ; i++) {
+            for (int j = 0; j <grid[0].length ; j++) {
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    void dfs(char[][] grid,int r,int c){
+        //数组越界就return
+        if(!inArea(grid,r,c)){
+            return;
+        };
+        //--------逻辑处理开始
+        if(grid[r][c]!='1'){
+            //如果不是岛屿没必要dfs直接return
+            return;
+        }
+
+        if(grid[r][c] == '1'){
+            //已经访问过置为0
+            grid[r][c] = '0';
+        }
+
+        //--------逻辑处理结束
+        //向上
+        dfs(grid,r-1,c);
+        //向左
+        dfs(grid,r,c-1);
+        //向右
+        dfs(grid,r,c+1);
+        //向下
+        dfs(grid,r+1,c);
+
+    }
+
+    public boolean inArea(char[][] grid,int r,int c){
+        return r>=0 && r<= grid.length-1 && c>=0 && c<=grid[0].length-1;
+    }
+
 
     public static void main(String[] args) {
         NumIslands numIslands = new NumIslands();
