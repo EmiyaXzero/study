d48664439dfdbf42025a552517fb9fccb2b30440
shanghang <9LxjeqhOxS5A>
2020-04-27 22:00:34 +0800

    StringPool测试以及学习的xmind

--- java/src/main/java/com/my/method/BooleanTest.java
+++ java/src/main/java/com/my/method/BooleanTest.java
@@ -7,18 +7,24 @@ package com.my.method;
  **/
 public class BooleanTest {
     public static void main(String[] args) {
-        byte a = 1;
-        byte b = -1;
-        Integer integer = new Integer(16);
-        Integer integer1 = Integer.valueOf(16);
-        Integer integer2 = Integer.valueOf(16);
-        Integer integer3 = new Integer(16);
-        System.out.println((integer == integer1));
-        System.out.println((integer1 == integer2));
-        System.out.println((integer2 == integer3));
-        System.out.println((integer == integer3));
-        //拆箱操作等同于integer1.intValue();
-        int int1 = integer1;
+//        byte a = 1;
+//        byte b = -1;
+//        Integer integer = new Integer(16);
+//        Integer integer1 = Integer.valueOf(16);
+//        Integer integer2 = Integer.valueOf(16);
+//        Integer integer3 = new Integer(16);
+//        System.out.println((integer == integer1));
+//        System.out.println((integer1 == integer2));
+//        System.out.println((integer2 == integer3));
+//        System.out.println((integer == integer3));
+//        //拆箱操作等同于integer1.intValue();
+//        int int1 = integer1;
 
+        String stringA = new String("bbb");
+        String stringB = stringA.intern();
+        String stringC = "bbb";
+        System.out.println((stringA == stringB));
+        System.out.println((stringA == stringC));
+        System.out.println((stringB == stringC));
     }
 }
