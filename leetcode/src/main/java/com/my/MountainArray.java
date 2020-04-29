b638c6a8caa202aca1bfc53d2daa1a56e727b68c
shanghang <825564615@qq.com>
2020-04-29 10:05:11 +0800

    4月29号二分搜索打卡

---
+++ leetcode/src/main/java/com/my/MountainArray.java
@@ -0,0 +1,14 @@
+package com.my;
+
+/**
+ * @author shang
+ * @title: MountainArray
+ * @projectName study
+ * @description: TODO
+ * @date 2020/4/29-9:08
+ */
+public interface MountainArray {
+    public int get(int index);
+
+    public int length();
+}
