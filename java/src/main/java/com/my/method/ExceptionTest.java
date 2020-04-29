185ff93171278ddd2bc7d8e55befbdb38a5738c0
shanghang <9LxjeqhOxS5A>
2020-04-29 23:45:45 +0800

    CS-Notes学习

---
+++ java/src/main/java/com/my/method/ExceptionTest.java
@@ -0,0 +1,13 @@
+package com.my.method;
+
+/**
+ * @Author: shanghang
+ * @Project:study
+ * @Date: 2020/4/29 23:18
+ **/
+public class ExceptionTest {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1/0;
+    }
+}
