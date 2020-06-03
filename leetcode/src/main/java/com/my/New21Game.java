595b0df473b94a3676eba52d53da5be11109d8dd
shanghang <9LxjeqhOxS5A>
2020-06-04 00:40:53 +0800

    新21点

--- leetcode/src/main/java/com/my/New21Game.java
+++ leetcode/src/main/java/com/my/New21Game.java
@@ -22,7 +22,7 @@ public class New21Game {
         if(K == 0){
             return 1;
         }
-        double[] dp = new double[W+1];
+        double[] dp = new double[K+W];
         //分数在k到 min(N,K+W-1)的时候直接获胜,K+W-1是最后一次抽到的分数为W且前一次是K-1因为只有前一次是K-1才不会退出游戏
         for (int i = K; i <= N && i < K + W; i++) {
             dp[i] = 1.0;
@@ -36,9 +36,36 @@ public class New21Game {
         return dp[0];
     }
 
+    public double new21GameNewDp(int N, int K, int W) {
+        if(K == 0){
+            return 1;
+        }
+        double[] dp = new double[K+W];
+        double s = 0;
+        //分数在k到 min(N,K+W-1)的时候直接获胜,K+W-1是最后一次抽到的分数为W且前一次是K-1因为只有前一次是K-1才不会退出游戏
+        for (int i = K; i < K + W; i++) {
+            dp[i] = (i <= N ? 1.0 : 0.0);
+            s += dp[i];
+        }
+        for (int i = K-1;i>=0;i--){
+            dp[i] = s/W;
+            s = s-dp[i+W]+dp[i];
+        }
+        /**
+         * 或者这样
+         *  dp[K-1] = s/W;
+         * for (int i = K-2;i>=0;i--){
+         *             s = s-dp[i+1+W]+dp[i+1];
+         *             dp[i] = s/W;
+         *         }
+         */
+
+        return dp[0];
+    }
+
     public static void main(String[] args) {
         New21Game new21Game = new New21Game();
-        new21Game.new21Game(6,1,10);
+        new21Game.new21GameNewDp(21,17,10);
     }
 
 }
