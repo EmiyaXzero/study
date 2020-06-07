b55f534bfa6dedd291ddfa1ae154614b9e81c852
shanghang <825564615@qq.com>
2020-06-07 13:54:40 +0800

    一次提交

--- leetcode/src/main/java/com/my/ProductExceptSelf.java
+++ leetcode/src/main/java/com/my/ProductExceptSelf.java
@@ -1,32 +1,18 @@
 package com.my;
 
 /**
- * @Author: shanghang
- * @Project:study
- * @description:238. 除自身以外数组的乘积
- * @Date: 2020/6/4 23:38
- **/
+ * @author shanghang
+ */
 public class ProductExceptSelf {
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-        int[] result = new int[nums.length];
-        left[0] = 1;
-        for (int i = 1;i<nums.length;i++){
-            left[i] = left[i-1]*nums[i-1];
+        int all = 1;
+        for (int num : nums){
+            all*=num;
         }
-        right[nums.length-1] = 1;
-        for (int i = nums.length-2;i>=0;i--){
-            right[i] = right[i+1]*nums[i+1];
-        }
+        int[] dp = new int[nums.length];
         for (int i = 0 ;i<nums.length;i++){
-            result[i] = left[i]*right[i];
+            dp[i] = all/nums[i];
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        ProductExceptSelf productExceptSelf = new ProductExceptSelf();
-        productExceptSelf.productExceptSelf(new int[]{1,2,3,4});
+        return dp;
     }
 }
